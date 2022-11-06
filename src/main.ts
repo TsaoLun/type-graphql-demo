77a92a9533e6a427faa5f6c791414f4bee0f9026
TsaoLun <ycl18@live.com>
2022-11-06 19:17:57 +0800

    complete simple demo

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,26 @@
-import "reflect-metadata"
-import { Field, ObjectType, Int, Float } from "type-graphql"
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server";
+import * as path from "path";
+import { buildSchema } from "type-graphql";
 
-@ObjectType({ description: "Object representing cooking recipe" })
-export class Recipe {
-  @Field()
-  title: string;
+import { RecipeResolver } from "./graphql/schema_resolver";
 
-  @Field(type => String, { nullable: true, deprecationReason: "Use `description` field instead" })
-  get specification(): string | undefined {
-    return this.description;
-  }
+async function bootstrap() {
+  // build TypeGraphQL executable schema
+  const schema = await buildSchema({
+    resolvers: [RecipeResolver],
+    // automatically create `schema.gql` file with schema definition in current folder
+    emitSchemaFile: path.resolve(__dirname, "graphql/schema.gql"),
+  });
 
-  @Field({ nullable: true, description: "The recipe description with preparation info" })
-  description?: string;
+  // Create GraphQL server
+  const server = new ApolloServer({
+    schema,
+  });
 
-  @Field(type => [Int])
-  ratings: number[];
-
-  @Field()
-  creationDate: Date;
-
-  @Field(type => Int)
-  ratingsCount: number;
-
-  @Field(type => Float, { nullable: true })
-  get averageRating(): number | null {
-    const ratingsCount = this.ratings.length;
-    if (ratingsCount === 0) {
-      return null;
-    }
-    const ratingsSum = this.ratings.reduce((a, b) => a + b, 0);
-    return ratingsSum / ratingsCount;
-  }
+  // Start the server
+  const { url } = await server.listen(4000);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
 }
+
+bootstrap();
